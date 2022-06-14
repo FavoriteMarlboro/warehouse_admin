6a0a0fa297631dea6ea4eccf97b478c0815cedc2
Milchstraße <3207191628@qq.com>
2022-06-14 17:10:47 +0800

    Second submission

---
+++ src/main/java/com/gx/warehouse_admin/WarehouseAdminApplication.java
@@ -0,0 +1,13 @@
+package com.gx.warehouse_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseAdminApplication.class, args);
+    }
+
+}
